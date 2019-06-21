2d987e22f3aa418393664170267baf86a52f92c4
Gilles Lautrou <g.lautrou@outlook.com>
2019-06-21 14:58:21 +0200

    Disable login button during HTTP cal

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -3,53 +3,58 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Text, View, Button, TextInput, ActivityIndicator } from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
+import React, { Component } from 'react';
+import { Text, View, Button, TextInput, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 type Props = {};
 class AuthScreen extends React.Component<Props> {
   static navigationOptions = {
-    title: "Please sign in"
+    title: 'Please sign in'
   };
 
   state = {
     loading: false,
-    username: "",
-    password: ""
+    username: '',
+    password: ''
   };
 
   render() {
     return (
       <View>
         <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
           onChangeText={username => this.setState({ username })}
           value={this.state.username}
-          textContentType={"username"}
+          textContentType={'username'}
           placeholder="Username"
         />
         <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
           onChangeText={password => this.setState({ password })}
           value={this.state.password}
-          textContentType={"password"}
+          textContentType={'password'}
           secureTextEntry={true}
           placeholder="Password"
         />
         {!this.state.loading && (
           <Button title="Sign in" onPress={this.signInAsync} />
         )}
-        {this.state.loading && <ActivityIndicator />}
+        {this.state.loading && (
+          <>
+            <Button title="Sign in" disabled={true} />
+            <ActivityIndicator />
+          </>
+        )}
       </View>
     );
   }
 
   signInAsync = async () => {
     this.setState({ loading: true });
-    await AsyncStorage.setItem("userToken", "abc");
+    await AsyncStorage.setItem('userToken', 'abc');
     this.setState({ loading: false });
-    this.props.navigation.navigate("App");
+    this.props.navigation.navigate('App');
   };
 }
 
